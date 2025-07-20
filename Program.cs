0bdd418a2f655b1d2d7cc02f4dfe02751b219513
unknown <maytomeze@gmail.com>
2025-07-20 17:41:41 +0300

    'task#1'

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_asp_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter random value \n");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"type : {y.GetType()}");
+        }
+    }
+}
